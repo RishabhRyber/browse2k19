8b3944db2e1ea26d6c71516d99a8aa058d3c2365
77CaptainClaw77 <36629432+77CaptainClaw77@users.noreply.github.com>
2019-03-06 00:10:20 +0530

    Menu Items Added,Overflow Removed

--- Browse 19/js/main.js
+++ Browse 19/js/main.js
@@ -15,7 +15,14 @@ function changeTab(id)
 {
   document.getElementById('MENU').style.display = 'none';
   document.getElementById('ABOUT').style.display = 'none';
+  document.getElementById('CYBER').style.display = 'none';
+  document.getElementById('MIND').style.display = 'none';
+  document.getElementById('LIFE').style.display = 'none';
+  document.getElementById('PHOTO').style.display = 'none';
+  document.getElementById('PRASTUTI').style.display = 'none';
+  document.getElementById('BYTE').style.display = 'none';
   document.getElementById(id).style.display = 'block';
   document.getElementById('trigger').click();
-  document.getElementById('menu_txt').innerHTML = id;
+  document.getElementById('menu_txt').innerHTML= id;
+  
 }
